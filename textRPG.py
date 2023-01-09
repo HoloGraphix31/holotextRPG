a3c70f35a57f32ec54958f805abab06292031795
HoloGraphix31 <115778950+HoloGraphix31@users.noreply.github.com>
2023-01-09 10:05:56 +0100

    Update textRPG.py

--- textRPG.py
+++ textRPG.py
@@ -22,9 +22,10 @@ def combat(stats_heros,stats_adversaire):
 
 '''
 #nom,hp,hp_max,atk,def,speed,dmg_rndm,dmg_fixe
+#actions: attaque,defense,soin
 
 def combat(nom_heros,hp_heros,hp_max_heros,attaque_heros,defense_heros,vitesse_heros,attaque_random_heros,attaque_fixe_heros,nom_ennemi,hp_ennemi,hp_max_ennemi,attaque_ennemi,defense_ennemi,vitesse_ennemi,attaque_random_ennemi,attaque_fixe_ennemi):
     if random.randint(20)+vitesse_heros > random.randint(20)+vitesse_ennemi:
-        #heros attaque en premier
+        while hp_heros > 0 and hp_ennemi > 0
     else :
         #ennemi attaque en premier
